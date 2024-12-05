fdac60636c2b8d9e35a7aaf3e2dcd76864fec8fa
viego-the-Ruined-King <190290542+viego-the-Ruined-King@users.noreply.github.com>
2024-12-05 13:15:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/(chat)/actions.ts
@@ -0,0 +1,42 @@
+'use server';
+
+import { type CoreUserMessage, generateText } from 'ai';
+import { cookies } from 'next/headers';
+
+import { customModel } from '@/lib/ai';
+import {
+  deleteMessagesByChatIdAfterTimestamp,
+  getMessageById,
+} from '@/lib/db/queries';
+
+export async function saveModelId(model: string) {
+  const cookieStore = await cookies();
+  cookieStore.set('model-id', model);
+}
+
+export async function generateTitleFromUserMessage({
+  message,
+}: {
+  message: CoreUserMessage;
+}) {
+  const { text: title } = await generateText({
+    model: customModel('gpt-4o-mini'),
+    system: `\n
+    - you will generate a short title based on the first message a user begins a conversation with
+    - ensure it is not more than 80 characters long
+    - the title should be a summary of the user's message
+    - do not use quotes or colons`,
+    prompt: JSON.stringify(message),
+  });
+
+  return title;
+}
+
+export async function deleteTrailingMessages({ id }: { id: string }) {
+  const [message] = await getMessageById({ id });
+
+  await deleteMessagesByChatIdAfterTimestamp({
+    chatId: message.chatId,
+    timestamp: message.createdAt,
+  });
+}
